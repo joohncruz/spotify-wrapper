de625a3b491653caaeccbc46cadfe48d417a3559
jonathan.cruz <jonathan.cruz@smarttbotter.com.br>
2018-08-22 18:12:06 -0300

    Criando smoke tests e usando sinonjs

--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
-import fetch from 'node-fetch';
-
 export const search = (query, type) => fetch(`https://api.spotify.com/v1/search?q=${query}&type=${type}`);
 export const searchAlbums = () => {};
 export const searchArtists = () => {};
